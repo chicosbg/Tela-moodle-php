cb2806c360b8549a3609141baab22822ab676ec6
chicosbg <luisfrancisco.15.brum@gmail.com>
2025-11-18 22:09:59 -0300

    cursos ativos

--- src/views/stats-cards.php
+++ src/views/stats-cards.php
@@ -12,4 +12,8 @@
         <div class="stat-number"><?php echo $notificacoes_nao_lidas; ?></div>
         <div class="stat-label">Notificações Não Lidas</div>
     </div>
+    <div class="stat-card">
+        <div class="stat-number"><?php echo count($estatisticas['cursos_ativos']); ?></div>
+        <div class="stat-label">Cursos Ativos</div>
+    </div>
 </div>
